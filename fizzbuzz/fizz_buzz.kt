92de9d65544a23852f154944d94d026f99a8b553
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-06-21 10:45:14 +0900

    Kotlin

---
+++ fizzbuzz/fizz_buzz.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>): Unit {
+
+    for (i in 1..100) {
+
+        when {
+            i % 15 == 0 -> println("FizzBuzz")
+            i % 3 == 0 -> println("Fizz")
+            i % 5 == 0 -> println("Buzz")
+            else -> println(i)
+        }
+
+    }
+}
