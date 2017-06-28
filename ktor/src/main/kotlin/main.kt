b7f4c92f8bc483243a3d85e432c8e0ce0157c499
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-06-28 11:27:41 +0900

    ことりん実験中

---
+++ ktor/src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.response.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
