a65da8c1ebd69902cfe4b0e3f5cd40050ef70ca5
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-01-17 12:27:39 +0900

    react

---
+++ react/webpack.config.js
@@ -0,0 +1,72 @@
+let path = require('path');
+let webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let dir = __dirname;
+let output_path = `${dir}/public/`;
+
+module.exports = [
+    {
+        cache: false,
+        devServer: {
+            contentBase: 'public'
+        },
+        context: path.join(__dirname, 'src/js'),
+        entry: {
+            bundle: `${dir}/src/entry.js`
+        },
+        output: {
+            path: output_path,
+            filename: "js/[name].min.js"
+        },
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new WebpackBuildNotifierPlugin({
+                title: "Webpack Build"
+            }),
+            new ExtractTextPlugin("css/[name].min.css"),
+            new CopyWebpackPlugin([
+                {from: `${dir}/node_modules/bootstrap/dist/css/bootstrap.min.css`, to: `${output_path}/css/bootstrap.min.css`}
+            ])
+        ],
+        resolve: {
+            modules: [
+                `${dir}/src/scripts`,
+                `${dir}/src/styles`,
+                "node_modules"
+            ]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: [
+                            ['env', {'modules': false}]
+                        ]
+                    }
+                },
+                {
+                    test: /\.jsx$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader?minimize!sass-loader"
+                    })
+                }
+            ]
+        }
+    }
+];
