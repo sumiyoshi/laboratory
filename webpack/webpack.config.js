9edf54d1c8c658e211f787f269329482733671f2
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-01-17 11:21:39 +0900

    webpack修正

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -42,7 +42,9 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015']
+                    presets: [
+                        ['env', {'modules': false}]
+                    ]
                 }
             },
             {
