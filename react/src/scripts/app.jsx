7ff1ef1810f2f6ab3c39d68324d904c1ec717490
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2018-01-17 12:52:31 +0900

    react router

--- react/src/scripts/app.jsx
+++ react/src/scripts/app.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-class App extends React.Component {
-    render () {
-        return <h1> Hello React!</h1>;
-    }
-}
+const App = () => (
+    <BrowserRouter>
+        <div>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/friends'>Friends</Link></li>
+            </ul>
 
-render(<App/>, document.getElementById('app'));
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About}/>
+            <Route path='/friends' component={Friends}/>
+        </div>
+    </BrowserRouter>
+);
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+        <p>Welcome to ようこそ</p>
+    </div>
+);
+const About = () => (
+    <div>
+        <h2>About</h2>
+        <p>フレンズに投票するページです</p>
+    </div>
+);
+const Friends = () => (
+    <div>
+        <h2>Friends</h2>
+        <p>ここにフレンズのリストを書きます</p>
+    </div>
+);
+
+export default App
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
